972f3a4fe01243a5bec375ae010304d75e33eb92
Edgar <edgar@onsist.com.br>
2022-09-02 15:58:42 -0300

    Add bootstrap

--- src/polls/models.py
+++ src/polls/models.py
@@ -13,7 +13,7 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     
     def was_published_recently(self):
-        now = timezone.now()
+        now = timezone.now()        
         return now - datetime.timedelta(days=1) <= self.pub_date <= now        
 
 
