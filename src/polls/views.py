c55881bcad809520260f2d88047ffad0a80d2b9a
Edgar <edgar@onsist.com.br>
2022-09-06 16:30:28 -0300

    Rota new adicionada

--- src/polls/views.py
+++ src/polls/views.py
@@ -62,3 +62,7 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+class NewView(generic.TemplateView):
+    model = Question
+    template_name = 'polls/new.html'
